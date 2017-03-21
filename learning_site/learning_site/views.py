33f350b39ce131aa75655278173d68fa1b61e117
mike-del <delgado.portland@gmail.com>
2017-03-21 14:43:52 -0700

    Updated Quiz view and model

--- learning_site/learning_site/views.py
+++ learning_site/learning_site/views.py
@@ -6,22 +6,23 @@ from django.shortcuts import render
 
 from . import forms
 
+
 def hello_world(request):
     return render(request, 'home.html')
 
 
 def suggestion_view(request):
-	form = forms.SuggestionForm()
-	if request.method == 'POST':
-		form = forms.SuggestionForm(request.POST)
-		if form.is_valid():
-			send_mail(
-				'Suggestion from {}'.format(form.cleaned_data['name']),
-				form.cleaned_data['suggestion'],
-				'{name} <{email}>'.format(**form.cleaned_data),
-				['delgado.portland@gmail.com']
-			)
-			messages.add_message(request, messages.SUCCESS,
-				'Thanks for your suggestions!')
-			return HttpResponseRedirect(reverse('suggestion'))
-	return render(request, 'suggestion_form.html', {'form': form})
+    form = forms.SuggestionForm()
+    if request.method == 'POST':
+        form = forms.SuggestionForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                'Suggestion from {}'.format(form.cleaned_data['name']),
+                form.cleaned_data['suggestion'],
+                '{name} <{email}>'.format(**form.cleaned_data),
+                ['kenneth@teamtreehouse.com']
+            )
+            messages.add_message(request, messages.SUCCESS,
+                                 'Thanks for your suggestion!')
+            return HttpResponseRedirect(reverse('suggestion'))
+    return render(request, 'suggestion_form.html', {'form': form})
